4f97b66420021dca00f90440a8cc5ba5e1d10b02
Adolph C <adolphc@outlook.com>
2020-05-29 13:03:36 -0400

    Removed pow/target and updated gitignore.

--- pow/target/debug/deps/pow-42543c032a8b4e94.d
+++
@@ -1,5 +0,0 @@
-/home/adolph/code/cargo-asm/pow/target/debug/deps/pow-42543c032a8b4e94: src/main.rs
-
-/home/adolph/code/cargo-asm/pow/target/debug/deps/pow-42543c032a8b4e94.d: src/main.rs
-
-src/main.rs:
--- pow/target/debug/pow.d
+++
@@ -1,1 +0,0 @@
-/home/adolph/code/cargo-asm/pow/target/debug/pow: /home/adolph/code/cargo-asm/pow/src/main.rs
